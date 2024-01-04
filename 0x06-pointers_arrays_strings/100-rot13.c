5a7b0e70f932f6549ff9b992a440004f2db382de
fadyfayez2004 <125869805+fadyfayez2004@users.noreply.github.com>
2024-01-05 00:05:03 +0200

    Add files via upload

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,27 @@
 #include "main.h"
 
 /**
-  * rot13 - function to encode
-  * Return: n after encryption
-  * @n: plaintext
-  *
-  *
-  *
-  */
-char *rot13(char *n)
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *rot13(char *s)
 {
 	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (n[i] == s1[j])
+			if (a[j] == *(s + i))
 			{
-				n[i] = s2[j];
+				*(s + i) = b[j];
+				break;
 			}
 		}
 	}
-
-	return (n);
+	return (s);
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,48 @@
 #include "main.h"
 
 /**
-  *
-  *
-  *
-  *
-  *
-  */
-
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * ahhh! Crazy task! Expand your knowledge
+ * Return: address of r or 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	long long i, j, k, num1, num2, digit;
+	int i, j, k, l, m, n;
 
-	/*finding the nunber of the first number*/
-	num1 = 0;
-	for (i = 0; n1[i] != '\0'; i++)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		digit = n1[i] - '0';
-		num1 = num1 * 10 + digit;
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-
-	/*finding the second number*/
-	num2 = 0;
-	for (j = 0; n2[j] != '\0'; j++)
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
 	{
-		digit = n2[i] - '0';
-		num2 = num2 * 10 + digit;
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
 	}
-
-	k = num1 + num2;
+	return (r);
+}
+/*Bwave */
